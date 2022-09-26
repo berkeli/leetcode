0da68e3669d35f1e5b154bf567b503250a74f632
Berkeli Halmyradov <berkelih@gmail.com>
2022-09-26 10:03:56 +0100

    Circular queue

---
+++ Queues/622-Design-Circular-Queue/main.go
@@ -0,0 +1,61 @@
+package main
+
+type MyCircularQueue struct {
+	data []int
+	size int
+}
+
+func Constructor(k int) MyCircularQueue {
+	return MyCircularQueue{
+		size: k,
+	}
+}
+
+func (this *MyCircularQueue) EnQueue(value int) bool {
+	if len(this.data) >= this.size {
+		return false
+	}
+	this.data = append(this.data, value)
+	return true
+}
+
+func (this *MyCircularQueue) DeQueue() bool {
+	if len(this.data) == 0 {
+		return false
+	}
+	this.data = this.data[1:]
+	return true
+}
+
+func (this *MyCircularQueue) Front() int {
+	if len(this.data) == 0 {
+		return -1
+	}
+	return this.data[0]
+}
+
+func (this *MyCircularQueue) Rear() int {
+	if len(this.data) == 0 {
+		return -1
+	}
+	return this.data[len(this.data)-1]
+}
+
+func (this *MyCircularQueue) IsEmpty() bool {
+	return len(this.data) == 0
+}
+
+func (this *MyCircularQueue) IsFull() bool {
+	return len(this.data) == this.size
+}
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * obj := Constructor(k);
+ * param_1 := obj.EnQueue(value);
+ * param_2 := obj.DeQueue();
+ * param_3 := obj.Front();
+ * param_4 := obj.Rear();
+ * param_5 := obj.IsEmpty();
+ * param_6 := obj.IsFull();
+ */
---
+++ Queues/622-Design-Circular-Queue/main_test.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+// Example 1:
+
+// Input
+// ["MyCircularQueue", "enQueue", "enQueue", "enQueue", "enQueue", "Rear", "isFull", "deQueue", "enQueue", "Rear"]
+// [[3], [1], [2], [3], [4], [], [], [], [4], []]
+// Output
+// [null, true, true, true, false, 3, true, true, true, 4]
+
+// Explanation
+// MyCircularQueue myCircularQueue = new MyCircularQueue(3);
+// myCircularQueue.enQueue(1); // return True
+// myCircularQueue.enQueue(2); // return True
+// myCircularQueue.enQueue(3); // return True
+// myCircularQueue.enQueue(4); // return False
+// myCircularQueue.Rear();     // return 3
+// myCircularQueue.isFull();   // return True
+// myCircularQueue.deQueue();  // return True
+// myCircularQueue.enQueue(4); // return True
+// myCircularQueue.Rear();     // return 4
+
+func TestMyCircularQueue(t *testing.T) {
+	t.Run("Example 1", func(t *testing.T) {
+		myCircularQueue := Constructor(3)
+		boolVal := myCircularQueue.EnQueue(1) // return True
+		require.True(t, boolVal)
+
+		boolVal = myCircularQueue.EnQueue(2) // return True
+		require.True(t, boolVal)
+
+		boolVal = myCircularQueue.EnQueue(3) // return True
+		require.True(t, boolVal)
+
+		boolVal = myCircularQueue.EnQueue(4) // return False
+		require.False(t, boolVal)
+
+		val := myCircularQueue.Rear() // return 3
+		require.Equal(t, 3, val)
+
+		boolVal = myCircularQueue.IsFull() // return True
+		require.True(t, boolVal)
+
+		boolVal = myCircularQueue.DeQueue() // return True
+		require.True(t, boolVal)
+
+		boolVal = myCircularQueue.EnQueue(4) // return True
+		require.True(t, boolVal)
+
+		val = myCircularQueue.Rear() // return 4
+		require.Equal(t, 4, val)
+	})
+}
